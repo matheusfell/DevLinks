0fb94213d3e19a28837a6d44eda5620c110d2bf6
Matheus Fellipe <bentomatheus48@gmail.com>
2023-01-31 01:49:03 -0300

    update

--- script.js
+++ script.js
@@ -2,17 +2,11 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
-
   const img = document.querySelector("#profile img")
 
-  if(html.classList.contains('light')) {
-    img.setAttribute('src', './assets/avatar-light.png')
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
   } else {
-    img.setAttribute('src', './assets/avatar.png')
+    img.setAttribute("src", "./assets/avatar.png")
   }
-
-  
-
-
 }
-
